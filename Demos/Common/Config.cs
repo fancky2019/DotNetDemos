fb3feef66c53c29f369db53028da15cedfc471b6
fancky2019 <fancky2019@users.noreply.github.com>
2018-12-26 11:15:13 +0800

    Demos

---
+++ Demos/Common/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    class Config
+    {
+
+        public static string ConStr =>  ConfigurationManager.AppSettings["ConStr"];
+        static Config()
+        {
+
+        }
+    }
+}
---
+++ Demos/Program.cs
@@ -0,0 +1,50 @@
+﻿using Demos.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+
+            //new ParamsDemo().Test();
+            // string str = Test().Result;
+            //   new AdoTest().Test();
+
+            //new TClassTest<Product>().Test();
+            new LockDemo().Test();
+            int m = 0;
+            Console.ReadLine();
+        }
+        static void Fundd()
+        {
+            Te();
+        }
+        static async void Te()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            string str = await Test();
+
+        }
+        static async Task<string> Test()
+        {
+
+            return await Task.Run(() =>
+               {
+                   Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                   Thread.Sleep(10 * 1000);
+                   Console.WriteLine("dssdsd");
+                   return "sdsdsd";
+               });
+
+        }
+    }
+}
