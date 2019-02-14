2020836d161fc19146aac9c9c345ff921978b4b7
fancky <709737588@qq.com>
2019-02-14 14:27:09 +0800

    父类执行子类重写的虚方法

---
+++ Demos/Demos2019/SubjectTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.Demos2019
+{
+    class SubjectTest
+    {
+        public void Test()
+        {
+            new B();//1,0
+        }
+    }
+    class A
+    {
+        public A()
+        {
+            PrintFields();
+        }
+        public virtual void PrintFields() { }
+    }
+    class B : A
+    {
+        int x = 1;
+        int y;
+        public B()
+        {
+            y = -1;
+        }
+        public override void PrintFields()
+        {
+            Console.WriteLine("x={0},y={1}", x, y);
+        }
+    }
+}
