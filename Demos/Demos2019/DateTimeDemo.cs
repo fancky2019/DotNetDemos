5dbd248c77f9f6f58a9e2ab34281b41455444e37
fancky <709737588@qq.com>
2019-05-20 11:12:00 +0800

    1

---
+++ Demos/Demos2019/DateTimeDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.Demos2019
+{
+    class DateTimeDemo
+    {
+        DateTime dt = DateTime.ParseExact("20190528", "yyyyMMdd", CultureInfo.InvariantCulture);
+    }
+}
