fc852faf6a12788b4c14bdfd01d971c2fee2737e
fancky <709737588@qq.com>
2019-05-27 17:02:31 +0800

    日期数字格式化

--- Demos/Demos2019/FormatDemo.cs
+++ Demos/Demos2019/FormatDemo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,23 +12,42 @@ namespace Demos.Demos2019
     /// </summary>
     class FormatDemo
     {
-        public void  Test()
+        public void Test()
         {
             Fun();
         }
 
-        private void  Fun()
+        private void Fun()
         {
+
+            var yearMonth = "1908";
+            //19
+            var yearNumber = yearMonth.Substring(0, 2);
+            //08
+            var monthNumber = yearMonth.Substring(2, 2);
+            var yearBegin = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss").Substring(0, 2);
+
+            var dateStr = $"{yearBegin}{yearNumber}{monthNumber}";
+            DateTime contractDate = DateTime.ParseExact(dateStr, "yyyyMM", CultureInfo.InvariantCulture);
+            DateTime oneMonthAgo = DateTime.Now.AddMonths(-1);
+
+
+
+            int i = 8;
+            var iStr = i.ToString("D2");//08
+
+            var m1 = "8".PadLeft(2, '0');//08
+            var m2 = "10".PadLeft(2, '0');//10
             string intStr = "52";
-            var str1=  Convert.ToDouble(intStr).ToString("0.00");
+            var str1 = Convert.ToDouble(intStr).ToString("0.00");
 
             double d = 10.434;
             var str2 = d.ToString("#0.00 ");//点后面几个0就保留几位
             var str3 = String.Format("{0:N2} ", d);//2位 
 
             var d1 = 52.00;
-           //去除小数点后无效0
-            string trimZero= d1.ToString("0.##");// .##表示最多保留2位有效数字
+            //去除小数点后无效0
+            string trimZero = d1.ToString("0.##");// .##表示最多保留2位有效数字
             var d2 = 52.10;
             string trimZero1 = d2.ToString("0.##");// .##表示最多保留2位有效数字
 
