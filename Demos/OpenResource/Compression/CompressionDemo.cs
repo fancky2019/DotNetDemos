411c09a569ea4486b6976730c4cd9a0c003fef48
fancky <709737588@qq.com>
2020-06-09 15:55:34 +0800

    MessagePack序列化

---
+++ Demos/OpenResource/Compression/CompressionDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.OpenResource.Compression
+{
+    class CompressionDemo
+    {
+        /*
+         * 几种压缩比较
+         * https://blog.csdn.net/leilonghao/article/details/73200859
+         * gzip压缩率最好，LZ4压缩速度最快
+         */
+    }
+}
