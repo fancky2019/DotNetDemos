917f1437f8b89689360bab28762f6958b28599af
fancky <709737588@qq.com>
2020-07-09 15:36:47 +0800

    特性使用

--- Demos/Demos2018/Reflection/JobAttribute.cs
+++ Demos/Demos2018/Reflection/JobAttribute.cs
@@ -6,13 +6,38 @@ using System.Threading.Tasks;
 
 namespace Demos.Demos2018.Reflection
 {
+    /*
+     * 使用：
+     * 一、利用构造函数传参
+      *[Job]
+      *[Job("Job1")]
+      *[Job("Job2", "1000", "")]
+      * 
+      * 二、利用属性赋值
+      *[Job(Name ="fancky",Salary ="1000",Description ="ke")]
+      *    
+      */
     //添加限定，只能作用于类上
     [AttributeUsage(AttributeTargets.Class)]
     class JobAttribute: Attribute
     {
-        public String Description { get; set; }
-        public JobAttribute(string description="")
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Description { get; set; }
+        public decimal Salary { get; set; }
+
+        public JobAttribute() : this("", 0, "")
         {
+
+        }
+        public JobAttribute(string name = "") : this(name, 0, "")
+        {
+        }
+        public JobAttribute(string name = "", int age = 0, string description = "")
+        {
+            this.Name = name;
+            this.Age = age;
             this.Description = description;
         }
     }
