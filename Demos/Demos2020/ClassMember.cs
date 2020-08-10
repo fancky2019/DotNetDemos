1071c1c22fafb92d6488bd276d9df934749a7a5f
fancky <709737588@qq.com>
2020-08-10 09:27:38 +0800

    json文件加注释

--- Demos/Demos2020/ClassMember.cs
+++ Demos/Demos2020/ClassMember.cs
@@ -16,5 +16,36 @@ namespace Demos.Demos2020
         //const 关键字用于修饰字段或局部变量的声明。 它指定字段或局部变量的值是常数，不能被修改
         //修饰字段或局部变量
         const int ORDER_ID = 4;
+        //volatile 关键字可应用于字段
+        volatile int m = 0;
+        //抽象方法不能在当前类中调用，要用虚方法
+        //抽象虚拟不能使私有的
+
+        //事件会广播：多个地方注册，都会收到回调。
+
+        public void Test()
+        {
+            string commandCode = "";
+            switch (commandCode)
+            {
+                case SwitchClass.CommandCode1:
+                    break;
+                case SwitchClass.CommandCode2:
+                    break;
+                //分支应该为常量
+                //case SwitchClass.CommandCode3:
+                //    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    class SwitchClass
+    {
+        public const string CommandCode1 = "1";
+        public const string CommandCode2 = "2";
+        public static string CommandCode3 = "3";
     }
 }
