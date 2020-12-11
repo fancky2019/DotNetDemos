bb2a880d7cb8a38f40e81d77e370d1614211d830
fancky2019 <709737588@qq.com>
2020-12-11 11:23:27 +0800

    ado.net、dapper参数

--- Demos/OpenResource/Dapper/DapperDemo.cs
+++ Demos/OpenResource/Dapper/DapperDemo.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Dapper;
+using Demos.Model;
 
 namespace Demos.OpenResource.Dapper
 {
+    /*
+     * github:https://github.com/StackExchange/Dapper
+     */
+
+
+
     /// <summary>
     /// A high performance Micro-ORM supporting SQL Server, MySQL, Sqlite, SqlCE, Firebird etc..
     /// </summary>
@@ -25,7 +33,9 @@ namespace Demos.OpenResource.Dapper
         }
         public void Test()
         {
-            Insert();
+            //Insert();
+
+            ParameterCommand();
         }
 
         private void Insert()
@@ -39,11 +49,40 @@ namespace Demos.OpenResource.Dapper
                 var insertCommand = $"insert into Person (Name) values ('message - {1}');";
                 connection.Execute(insertCommand);
 
-             
+
                 //connection.Query<Person>("select *  from Person");
             }
             stopwatch.Stop();
             Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+
+        private void ParameterCommand()
+        {
+            string insertCommand = $@"INSERT INTO [Demo].[dbo].[Person]([Name],[Age])
+                                      VALUES (@Name,@Age)";
+            var conString = "server=.;database=Demo;user=sa;pwd=123456";
+            Person person = new Person();
+            person.Name = "li";
+            person.Age = 25;
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+
+                var obj = new { Name = "匿名", Age = 27 };
+                var r1 = connection.Execute(insertCommand, obj);
+
+
+                var r2 = connection.Execute(insertCommand, person);
+
+
+                DynamicParameters dynamicParameters = new DynamicParameters();
+                //注意参数名前没有@,ado.net 的SqlParameter是要加@的。详情见AdoTest
+                dynamicParameters.Add("Name", "dynamicParameters");
+                dynamicParameters.Add("Age", 28);
+                var r3 = connection.Execute(insertCommand, dynamicParameters);
+
+
+                //connection.Query<Person>("select *  from Person");
+            }
         }
 
     }
