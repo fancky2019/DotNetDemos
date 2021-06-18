fbec5a6f779879e6502ce2a50b377d9547566bc2
fancky2019 <709737588@qq.com>
2021-06-18 16:24:56 +0800

    DataTable  select

---
+++ Demos/Demos2021/DataTableDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.Demos2021
+{
+    class DataTableDemo
+    {
+        public void Test()
+        {
+
+        }
+
+        private void  Fum()
+        {
+            DataTable dt = new DataTable();
+            List<string> sysUserGuidList = dt.AsEnumerable().Select(c => c.Field<string>("SysUserGuid")).ToList();
+        }
+    }
+}
