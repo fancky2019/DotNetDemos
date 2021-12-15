2b9089a1a1d090aaa7eb72e8bd0a9efe7c1ead49
fancky2019 <709737588@qq.com>
2021-12-15 11:24:10 +0800

    重载

---
+++ Demos/Demos2021/OverLoadDemo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos.Demos2021
+{
+    /*
+     * C#访问修饰符对重载没有影响，java继承的重载不能比父类的访问修饰符窄
+     */
+    internal class OverLoadDemo:OverLoadParent
+    {
+        //编译通过
+        //protected string GetName()
+        //{
+        //    return "name";
+        //}
+
+        //编译通过
+        //public string GetName()
+        //{
+        //    return "name";
+        //}
+
+        //编译通过
+        private string GetName()
+        {
+            return "name";
+        }
+    }
+
+    internal class OverLoadParent
+    {
+        protected string GetName ()
+        {
+            return "name";
+        }
+            
+    }
+}
